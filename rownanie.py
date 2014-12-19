84f50a1b05762a40fbc08c5a021d0bdb1ddf1dac
pkos <pkos@op.pl>
2014-12-19 22:14:50 +0100

    First file

---
+++ rownanie.py
@@ -0,0 +1,17 @@
+__author__ = 'skos'
+
+if __name__ == "__main__":
+    pass
+    try:
+        x = float(raw_input("Podaj x \n"))
+        print x
+
+        print type(x)
+        y = 3 * x + 4
+        print "Wynik rownania to %s" % str(y)
+
+
+
+    except:
+        print "Zla liczba. podaj inna liczbe"
+
