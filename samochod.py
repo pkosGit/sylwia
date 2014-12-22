26b5355a348433ef0389c34bdde95c97b1342aa5
pkos <pkos@op.pl>
2014-12-22 23:57:59 +0100

    Autka

--- samochod.py
+++ samochod.py
@@ -1,6 +1,6 @@
 __author__ = 'Sylwia'
 
-class Auto():
+class Auto(object):
     def __init__(self, m, n, p):
         self.model = m
         self.numer_rejestracyjny = n
@@ -21,6 +21,25 @@ class Auto():
         else:
             return "Za pare km zlom"
 
+class Osobowe(Auto):
+
+    def __init__(self, m, n, p, d=5, r =4, t="suw"):
+        super(Osobowe, self).__init__(m, n, p)
+        self.drzwi = d
+        self.rozstaw_osi = r
+        self.typ = t
+
+    def wyswietl(self):
+        print "Ilosc drzw : %s" %self.drzwi
+        print "Rozstaw osi wynosi %s" % self.rozstaw_osi
+        print "Typ : %s" % self.typ
+        print "Wymiana oleju %s"% self.wymiana()
+
+    def wymiana(self):
+        if self.przebieg % 20000 == 0:
+            return True
+        else:
+            return False
 
 auto1 = Auto("Audi", "RZE 12345", 52800)
 auto2 = Auto("Fiat", "EL542134", 0)
@@ -29,3 +48,6 @@ auto3 = Auto("Ford", "ZUSIA", 7878945231)
 auto1.wyswietl()
 auto2.wyswietl()
 auto3.wyswietl()
+
+SantaFe = Osobowe("SantaFe","jhsdfdsg", 4447562)
+SantaFe.wyswietl()
