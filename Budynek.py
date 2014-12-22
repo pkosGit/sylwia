547ff496aedcb91bafcfea0fdf7fae102bcaf4c4
pkos <pkos@op.pl>
2014-12-22 21:58:52 +0100

    Gra statki

---
+++ Budynek.py
@@ -0,0 +1,15 @@
+__author__ = 'Sylwia'
+
+class Budynek():
+    def __init__(self, adres):
+        self.__adres = adres
+        self.__wlasciciel
+
+
+budynek1 = Budynek("Waclawa 53/77")
+budynek2 = Budynek("Nowy Borek 156")
+
+
+print budynek1.__adres
+
+
